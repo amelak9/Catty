198ea36bb230c2bbbd73a9746760f9981ca41f70
Amela Kurtic <amela.kurtic@student.tugraz.at>
2021-03-19 00:55:41 +0100

    CATTY-543-PPPP-Recently-used-Bricks

--- src/CattyTests/Bricks/BrickManager/BrickManagerTests.swift
+++ src/CattyTests/Bricks/BrickManager/BrickManagerTests.swift
@@ -25,13 +25,46 @@ import XCTest
 @testable import Pocket_Code
 
 final class BrickManagerTests: XCTestCase {
-
+    
     func testSizeForBrick() {
         let brick = SetVariableBrick()
         let brickCell = brick.brickCell()
-
+        
         let size = BrickManager.shared()?.size(forBrick: brick)
         XCTAssertEqual(UIScreen.main.bounds.size.width, size?.width)
         XCTAssertEqual(brickCell?.cellHeight(), size?.height)
+    }
+    
+    func testRecentlyUsedBricks() {
+        let script = Script()
+        let setVariableBrick = SetVariableBrick()
+        script.add(setVariableBrick, at: 0)
+        let askBrick = AskBrick()
+        script.add(askBrick, at: 1)
+        let setBackgroundBrick = SetBackgroundBrick()
+        script.add(setBackgroundBrick, at: 2)
+        let nextLookBrick = NextLookBrick()
+        script.add(nextLookBrick, at: 3)
+        let previousLookBrick = PreviousLookBrick()
+        script.add(previousLookBrick, at: 4)
+        let setLookBrick = SetLookBrick()
+        script.add(setLookBrick, at: 5)
+        let penDownBrick = PenDownBrick()
+        script.add(penDownBrick, at: 6)
+        let penUpBrick = PenUpBrick()
+        script.add(penUpBrick, at: 7)
+        let setRotationStyleBrick = SetRotationStyleBrick()
+        script.add(setRotationStyleBrick, at: 8)
+        let stitchBrick = StitchBrick()
+        script.add(stitchBrick, at: 9)
+        let setLookByIndexBrick = SetLookByIndexBrick()
+        script.add(setLookByIndexBrick, at: 10)
+        
+        if let favourites = BrickManager.shared()?.selectableBricks(for: kBrickCategoryType(rawValue: 0)!) {
+            for favourite in favourites {
+                //TODO: Make sure that the last 10 used bricks are in the array
+            }
+            
+        }
     }
 }
